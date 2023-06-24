d1447b299d98fbaa9b1bbc1201a35512dd155a28
Tamima Sigma <1630436@iub.edu.bd>
2023-06-24 15:56:24 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,16 +21,19 @@ const books = [
 
 
 function BookList() {
-    return (
-      <section className='booklist'>
-        {/* <EventExamples/> */}
-        {books.map((book) => {
-          return <Book {...book} key={book.id} />;
-        })}
-      </section>
-    );
+  const someValue = 'shakeAndBake';
+  const displayValue = () => {
+    console.log(someValue);
   };
-
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        return <Book {...book} key={book.id} displayValue={displayValue} />;
+      })}
+    </section>
+  );
+}
+  
 //   const EventExamples =() =>{
 //   //  const handleFormInput =(e) =>
 //   //  {
@@ -63,22 +66,20 @@ function BookList() {
 //   );
 // };
   
-  const Book = (props) => {
-    const { img, title, author } = props;
-    const displaytitle=()=>{
-      console.log(title);
-    }
+const Book = (props) => {
+  const { img, title, author, displayValue } = props;
+    // const displaytitle=()=>{
+    //   console.log(title);
+   
     return (
       <article className='book'>
-        <img src={img} alt={title} />
-        <h2>{title}</h2>
-        <button onClick={displaytitle}>
-         displaytitle
-        </button>
-        <h4>{author} </h4>
-      </article>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <button onClick={displayValue}>click me</button>
+      <h4>{author} </h4>
+    </article>
     );
-  };
+  }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<BookList/>)
