f71800ffc620947a4ecdb4adc9ceb0193877dc79
Tamima Sigma <1630436@iub.edu.bd>
2023-06-24 15:44:06 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const books = [
 function BookList() {
     return (
       <section className='booklist'>
-        <EventExamples/>
+        {/* <EventExamples/> */}
         {books.map((book) => {
           return <Book {...book} key={book.id} />;
         })}
@@ -31,45 +31,50 @@ function BookList() {
     );
   };
 
-  const EventExamples =() =>{
-   const handleFormInput =(e) =>
-   {
-    console.log(e);
-    console.log(e.target.name);
-    console.log(e.target.value);
-   // console.log('input form button');
-   };
-   const handleButtonClick=()=>{
-    alert('Handle button click');
-   }
-   const handleFormSubmission =(e) =>
-   {
-    e.preventDefault();
-    console.log('form submitted');
-   }
-     return <section>
-      <form onSubmit={handleFormSubmission}> 
-        <h2>
-          Typical Form
-        </h2>
-        <input 
-        type="text"
-         name="example"
-          onChange={handleFormInput}
-           style={{margin: '1rem'}}/>
-           
-        <button onClick={handleButtonClick}>Click me</button>
-        <div><button onClick={handleFormSubmission}>Click me</button></div> 
-      </form>
-    </section>
-  };
+//   const EventExamples =() =>{
+//   //  const handleFormInput =(e) =>
+//   //  {
+//   //   console.log(e);
+//   //   console.log(e.target.name);
+//   //   console.log(e.target.value);
+//   //  // console.log('input form button');
+//   //  };
+//   //  const handleButtonClick=()=>{
+//   //   alert('Handle button click');
+//   //  }
+//   //  const handleFormSubmission =(e) =>
+//   //  {
+//   //   e.preventDefault();
+//   //   console.log('form submitted');
+//   //  }
+//   return (
+//     <section>
+//       <form>
+//         <h2>Typical Form</h2>
+//         <input
+//           type='text'
+//           name='example'
+//           onChange={(e) => console.log(e.target.value)}
+//           style={{ margin: '1rem 0' }}
+//         />
+//       </form>
+//       <button onClick={() => console.log('you clicked me')}>click me</button>
+//     </section>
+//   );
+// };
   
   const Book = (props) => {
     const { img, title, author } = props;
+    const displaytitle=()=>{
+      console.log(title);
+    }
     return (
       <article className='book'>
         <img src={img} alt={title} />
         <h2>{title}</h2>
+        <button onClick={displaytitle}>
+         displaytitle
+        </button>
         <h4>{author} </h4>
       </article>
     );
