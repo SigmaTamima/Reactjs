65a56eb830ca818c3eb93fc0cafd45ea4500834b
Tamima Sigma <1630436@iub.edu.bd>
2023-06-24 14:19:24 +0600

    update 2

--- src/index.js
+++ src/index.js
@@ -21,27 +21,59 @@ const books = [
 
 
 function BookList() {
-  return <section className='booklist'>
-    {books.map((book)=>{
-        const {img, title, author, id} =book
-         return <Book img= {img} title= {title} author={author} key={id}/>   
-    })}
-  </section>;
-}
+    return (
+      <section className='booklist'>
+        <EventExamples/>
+        {books.map((book) => {
+          return <Book {...book} key={book.id} />;
+        })}
+      </section>
+    );
+  };
 
-const Book = (props)=>
-{  const {img, title,author, children} = props;
-    return ( 
-        <article className=" book">
-            <img src={img} alt={title}/>
-            <h2>{title}</h2>
-            <h4 > {author} </h4>
-            {children}
-          
-        </article>
-    )
-};
-
+  const EventExamples =() =>{
+   const handleFormInput =(e) =>
+   {
+    console.log(e);
+    console.log(e.target.name);
+    console.log(e.target.value);
+   // console.log('input form button');
+   };
+   const handleButtonClick=()=>{
+    alert('Handle button click');
+   }
+   const handleFormSubmission =(e) =>
+   {
+    e.preventDefault();
+    console.log('form submitted');
+   }
+     return <section>
+      <form onSubmit={handleFormSubmission}> 
+        <h2>
+          Typical Form
+        </h2>
+        <input 
+        type="text"
+         name="example"
+          onChange={handleFormInput}
+           style={{margin: '1rem'}}/>
+           
+        <button onClick={handleButtonClick}>Click me</button>
+        <div><button onClick={handleFormSubmission}>Click me</button></div> 
+      </form>
+    </section>
+  };
+  
+  const Book = (props) => {
+    const { img, title, author } = props;
+    return (
+      <article className='book'>
+        <img src={img} alt={title} />
+        <h2>{title}</h2>
+        <h4>{author} </h4>
+      </article>
+    );
+  };
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<BookList/>)
