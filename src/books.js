147a2d3ba5669de3bb06d091e0c23b8fef77240f
Tamima Sigma <1630436@iub.edu.bd>
2023-06-28 16:21:45 +0600

    Fundamentals-updated

---
+++ src/books.js
@@ -0,0 +1,31 @@
+import image1 from './images/image1.jpg'
+import image2 from './images/image2.jpg'
+import image3 from './images/image3.jpg'
+import image4 from './images/image4.jpg'
+export const books = [
+
+    {
+        title :'Iron Flame ',
+        author: 'Rebecca Yarros',
+        img : image1,
+         id: 1,
+       },
+       {
+           title :'Atomic Habits',
+           author :'James Clear ',
+           img : image2,
+           id : 2
+        },
+        {
+            title :'The Covenant of Water ',
+            author :'Abraham Verghese',
+            img : image3,
+            id : 3
+         },
+         {
+            title :'Verity',
+            author :'Colleen Hoover',
+            img : image4,
+            id : 4
+         }
+]
--- src/index.js
+++ src/index.js
@@ -1,41 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import './index.css';
-const books = [
-
-    {
-        title :'Iron Flame ',
-        author: 'Rebecca Yarros',
-        img : './images/1_.jpg',
-         id: 1,
-       },
-       {
-           title :'Atomic Habits',
-           author :'James Clear ',
-           img : './images/2.jpg',
-           id : 2
-        }
-]
-
-
+import { books } from "./books";
+import { Book } from "./Book";
 
 function BookList() {
   // const someValue = 'shakeAndBake';
   // const displayValue = () => {
   //   console.log(someValue);
   // };
-const getBook=(id)=>{
-  const book=books.find((book)=>book.id===id);
-  console.log(book)
-}
+
   return (
+    <>
+    <h1>
+      Best Sellers
+    </h1>
     <section className='booklist'>
-      {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
+      {books.map((book,index) => {
+        return <Book {...book} key={book.id} number={index}/>;
       })}
     </section>
-  );
+    </>
+  ); 
 }
   
 //   const EventExamples =() =>{
@@ -70,24 +56,7 @@ const getBook=(id)=>{
 //   );
 // };
   
-const Book = (props) => {
-  const { img, title, author,getBook,id} = props;
-    // const displaytitle=()=>{
-    //   console.log(title);
 
-    const getSinglBook= () =>{
-      getBook(id)
-    }
-   
-    return (
-      <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={getSinglBook}>click me</button>
-      <h4>{author} </h4>
-    </article>
-    );
-  }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<BookList/>)
